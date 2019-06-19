d417467d81c1b027032c57aaec96db8be9ed9b8f
junpei onizawa <onizawajunpei4@gmail.com>
2019-06-19 13:53:10 +0900

    カラムの作成指示,modelをModelに（tclone/index/models.py）

--- tclone/index/models.py
+++ tclone/index/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 #index_newtweetテーブルに、指定のカラムの作成指示を出すことが可能になる
-class NewTweet(models.model):
+class NewTweet(models.Model):
 	tweet = models.CharField(max_length = 255)
